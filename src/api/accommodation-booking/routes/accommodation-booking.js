755cf4ca3add5e00f491bcf96cf82870dac8a2ed
Kond94 <33941010+Kond94@users.noreply.github.com>
2023-03-26 15:43:02 +0200

    remove accom booking

--- src/api/accommodation-booking/routes/accommodation-booking.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * accommodation-booking router.
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::accommodation-booking.accommodation-booking');
--- src/api/accommodation-booking/services/accommodation-booking.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * accommodation-booking service.
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::accommodation-booking.accommodation-booking');
