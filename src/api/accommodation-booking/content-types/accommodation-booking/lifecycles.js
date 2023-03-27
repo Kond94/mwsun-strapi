7054153149ed7f75161cd2ab4853b35b7cebc59f
Kond94 <33941010+Kond94@users.noreply.github.com>
2023-03-27 07:55:05 +0200

    Add Email Confirmation After Booking

--- src/api/accommodation-booking/content-types/accommodation-booking/lifecycles.js
+++ src/api/accommodation-booking/content-types/accommodation-booking/lifecycles.js
@@ -47,7 +47,22 @@ module.exports = {
                        <br />`,
       });
 
-      console.log("result: ", result);
+      strapi.plugins["email"].services.email.send({
+        to: email,
+        from: "frontoffice@malawisunhotel.com", // e.g. single sender verification in SendGrid
+        cc: "marketingmwsun@gmail.com",
+        bcc: "carolinemkutumula@gmail.com",
+        replyTo: "",
+        subject: "(TEST ENVIRONMENT) Thank you for your reservation",
+        text: "Hello", // Replace with a valid field ID
+        html: `
+                       Dear: ${firstName}
+                       <br />
+                       We have successfully received your reservation request. Our team will respond with a confirmation shortly.
+                       <br />
+                       We take pride in our customer care. If you are not reponded to within 15 minuites please talk to one of our Managers to help us serve you better
+                      `,
+      });
     } catch (err) {
       console.log(err);
     }
