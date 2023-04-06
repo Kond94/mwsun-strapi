5c21cc0ce1444b1f30fc0eed078460cf5d1d429d
Kondwerani Kamsesa <kamsesakond@hotmail.com>
2023-04-06 15:42:10 +0200

    Test Controller extend

--- src/api/banquet-booking/controllers/banquet-booking.js
+++ src/api/banquet-booking/controllers/banquet-booking.js
@@ -1,9 +1,22 @@
-'use strict';
+"use strict";
 
 /**
  *  banquet-booking controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::banquet-booking.banquet-booking');
+module.exports = createCoreController(
+  "api::banquet-booking.banquet-booking",
+  ({ strapi }) => ({
+    async findOne(ctx) {
+      const sanitizedQueryParams = await this.sanitizeQuery(ctx);
+      const { results, pagination } = await strapi
+        .service("api::banquet-booking.banquet-booking")
+        .find(sanitizedQueryParams);
+      const sanitizedResults = await this.sanitizeOutput(results, ctx);
+      console.log("From Controller", sanitizedResults);
+      return this.transformResponse(sanitizedResults, { pagination });
+    },
+  })
+);
